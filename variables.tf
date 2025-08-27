003ea6b12e52be1337e5ec463e2c8ae93082a332
Fabrice Semti <fabrice@fabricesemti.com>
2025-08-27 19:59:07 +0100

    feat: Add Terraform configuration for DigitalOcean droplet
    This commit introduces Terraform configuration files to provision a
    DigitalOcean droplet. It includes:
    
    - `provider.tf`: Configures the DigitalOcean provider.
    - `variables.tf`: Defines variables for the DigitalOcean token and
      private key path.
    - `data.tf`: Retrieves the DigitalOcean SSH key.
    - `wwww-1.tf`: Defines the DigitalOcean droplet resource.
    - `.gitignore`: Adds Terraform-related files to the ignore list.
    
    The droplet is configured with Ubuntu 22.04, Nginx, and an SSH key for
    access. The `user_data` section installs and starts Nginx.

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "do_token" {
+    description = "Digital Ocean API token"
+    type = string
+}
+variable "pvt_key" {
+    description = "Path to private SSH key (corresponding to the public key uploaded to Digital Ocean)"
+    type = string
+}
